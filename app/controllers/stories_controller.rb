df1f9eca2f203366e43bbbe953c55e1e7309eeb3
Matthew Reuther <matthew.reuther@gmail.com>
2015-06-24 11:29:48 -0500

    cleaned and updated routes, added haml to gemfile, and generated a stories controller

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,18 @@
+class StoriesController < ApplicationController
+  def index
+  end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(story_params)
+  end
+
+  private
+
+  def story_params
+    params.require(:story).permit(:title, :description)
+  end
+end
